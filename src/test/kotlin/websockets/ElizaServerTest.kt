74c3c1713e1adb9b799d070c576feed6ec8ad2cf
Pilar Ester <pilaresterdeval@gmail.com>
2022-11-14 09:02:03 +0100

    Update ElizaServerTest.kt

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -36,7 +36,7 @@ class ElizaServerTest {
         assertEquals("The doctor is in.", list[0])
     }
 
-    @Disabled
+    //@Disabled
     @Test
     fun onChat() {
         val latch = CountDownLatch(4)
@@ -45,8 +45,8 @@ class ElizaServerTest {
         val client = ElizaOnOpenMessageHandlerToComplete(list, latch)
         container.connectToServer(client, URI("ws://localhost:$port/eliza"))
         latch.await()
-        // assertEquals(XXX, list.size) COMPLETE ME
-        // assertEquals(XXX, list[XXX]) COMPLETE ME
+        assertEquals(4, list.size)
+        assertEquals("Please don't apologize.", list[3])
     }
 }
 
@@ -66,8 +66,8 @@ class ElizaOnOpenMessageHandlerToComplete(private val list: MutableList<String>,
     fun onMessage(message: String, session: Session) {
         list.add(message)
         latch.countDown()
-        // if (COMPLETE ME) {
-        //    COMPLETE ME
-        // }
+        if (list.size > 3) {
+            session.basicRemote.sendText("sorry")
+        }
     }
 }
